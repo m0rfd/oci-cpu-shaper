7717fbade54074c09e51e704d7637bdbe18f42fb
senomorf <arsenij.c.o@gmail.com>
2025-11-16 13:43:06 +0000

    Log CPU hog startup for self-hosted workflow

--- tests/integration/cmd/cpu-hog/main.go
+++ tests/integration/cmd/cpu-hog/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"math"
+	"os"
 	"runtime"
+	"runtime/debug"
 	"sync"
 	"time"
 )
@@ -19,6 +22,17 @@ func main() {
 	workers := flag.Int("workers", runtime.NumCPU(), "number of busy loop workers to launch")
 
 	flag.Parse()
+
+	debug.SetTraceback("all")
+
+	fmt.Fprintf(
+		os.Stderr,
+		"cpu-hog start duration=%s workers=%d goarch=%s\n",
+		duration,
+		*workers,
+		runtime.GOARCH,
+	)
+	defer fmt.Fprintf(os.Stderr, "cpu-hog exit\n")
 
 	if *workers <= 0 {
 		*workers = 1
